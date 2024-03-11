6c82531162ec348a1f8c6b4fa93b3f379f530713
yeongzzi <yeongzzzzi@gmail.com>
2024-03-11 18:34:24 +0900

    style: 불필요한 css 제거

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { ViteFaviconsPlugin } from "vite-plugin-favicon"; 파비콘 플러그인 추가 [24-02-26]
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 파비콘 플러그인 추가 [24-02-26]
+    // ViteFaviconsPlugin({
+    //   logo: "public/images/favicon.png"
+    // })
+  ],
 
   // 새로 추가
   server: {
